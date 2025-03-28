ca99b118ccaff69db5ffcd3c0809878777ba36e3
kbola98 <52285719+KBola98@users.noreply.github.com>
2025-03-28 11:27:22 +0530

    Created the Question Bank by looping all the raw data and saving it as a list of Question Objects

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-class Question:
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
-        
+from question_model import Question
+from data import question_data
+
+#Implementing the for loop to append the Question_Bank List
+
+question_bank = []
+
+for x in question_data:
+    qtext= x["text"]
+    answer = x["answer"]
+    
+    question = Question(qtext, answer)
+    
+    question_bank.append(question)
+    
+
+print(question_bank[1],answer)
